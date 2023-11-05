b887f5e4d0dc48a71863f83633fcd93efba35473
Midhun Balachandran <midhunbalachandran07@gmail.com>
2023-11-05 18:29:43 +0530

    created a 'sign_up.html' file in account app. Updated the urls.py and views.py. Extended the 'account.html' file to 'sign_up.html' file.

--- teetrend/account/urls.py
+++ teetrend/account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('account/' , views.account , name='account') ,
+  path('account/sign_up/' , views.sign_up , name='sign_up') ,
 ]
--- teetrend/account/views.py
+++ teetrend/account/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def account(request):
   return render(request , 'account_details.html')
+
+def sign_up(request):
+  return render(request , 'sign_up.html')  
